4833fe4243b5c0e2a9407fc56579ee3abfd2d286
M4T <mateo.lesiuk@gmail.com>
2019-02-18 14:02:25 +0100

    Add Github and UI controller

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,28 @@
+import Github from './Github';
+import * as UI from './UI';
+
 require('../css/style.scss');
+
+const searchBar = document.querySelector('#username-input');
+
+const userGithub = new Github();
+
+searchBar.addEventListener('keyup', async e => {
+  // if (e.keyCode !== 13) return;
+  // Taking a text from input
+  const textInput = e.target.value;
+
+  if (textInput !== '') {
+    // Calling method on Github class to get user
+    const user = await userGithub.getUserProfile(textInput);
+
+    if (user) {
+      UI.cleanUI();
+      UI.showProfile(user);
+      UI.showRepos(user);
+    }
+  } else {
+    // console.log('Cleaning html');
+    UI.cleanUI();
+  }
+});
